b0238f1484053fd8a3906c793b3b52ad4e8be9ad
djbyron <dan@byron.me>
2024-01-24 11:21:24 -0500

    Update 500-nginx.sh

--- scripts/500-nginx.sh
+++ scripts/500-nginx.sh
@@ -36,7 +36,8 @@ mkdir $web_root/example.com/wwwroot
 echo "Creating a default index.cfm"
 
 #ja tem o arquivo tb
-echo "<!doctype html><html><body><h1>Hello</h1><p><cfoutput>#now()</cfoutput></p></body></html>" > $web_root/default/wwwroot/index.cfm
+echo "<!doctype html><html><head><title>Test HTML Page</title></head><body><h1>Hello</h1></body></html>" > $web_root/default/wwwroot/index.html
+echo "<!doctype html><html><head><title>Test CFML Page</title></head><body><h1>Hello</h1><p><cfoutput>#now()</cfoutput></p></body></html>" > $web_root/default/wwwroot/index.cfm
 
 #add user tomcat to www-data group so it can read files
 #usermod -aG www-data tomcat
